a5bfc510c649718f31068e3715eb80ed2d31b5c8
Lucas Maia <lucasm@posteo.de>
2026-04-10 14:58:32 +0200

    feat(ex00): add Makefile and megaphone.cpp

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/09 15:24:22 by lbueno-m          #+#    #+#             */
+/*   Updated: 2026/04/09 15:24:24 by lbueno-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	std::string result;
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::string arg = argv[i];
+		for (int j = 0; j < (int)arg.length(); j++)
+		{
+			char c = toupper(arg[j]);
+			result += c;
+		}
+	}
+	std::cout << result << std::endl;
+	return 0;
+}
