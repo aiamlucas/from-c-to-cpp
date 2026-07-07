f61becb4b1379a4985206fbd89fdd10f5d72a60d
Lucas Maia <lucasm@posteo.de>
2026-07-07 16:04:48 +0200

    style: change grammar in comment

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -4,5 +4,5 @@ Zombie *zombieHorde(int N, std::string name) {
     Zombie *horde = new Zombie[N]; // single allocation -> N zombies on the heap
     for (int i = 0; i < N; i++)
         horde[i].setName(name);
-    return horde; // returning a pointere to the first element
+    return horde; // returning a pointer to the first element
 }
