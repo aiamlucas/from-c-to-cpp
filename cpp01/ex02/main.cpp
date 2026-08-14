ce897a42cbcef54fac1e390514aec3eeb8d84b21
Lucas Maia <lucasm@posteo.de>
2026-08-15 01:52:05 +0200

    docs(cpp01) update inline comments

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 19:17:41 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/07/09 19:17:44 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/08/14 21:10:05 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int main(void) {
     std::string *stringPTR = &brain;
     std::string &stringREF = brain;
 
-    std::cout << &brain << std::endl;     // adress of brain
-    std::cout << stringPTR << std::endl;  // adress stored in stringPTR
-    std::cout << &stringREF << std::endl; // adress of what stringREF refers to
+    std::cout << &brain << std::endl;     // address of brain
+    std::cout << stringPTR << std::endl;  // address stored in stringPTR
+    std::cout << &stringREF << std::endl; // address of what stringREF refers to
     std::cout << brain << std::endl;      // value of brain
     std::cout << *stringPTR << std::endl; // value pointed to by stringPTR
     std::cout << stringREF << std::endl;  // value of stringREF
