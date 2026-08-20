bfa7fcc1a56a44431b585a1d9614087840ae075f
Lucas Maia <lucasm@posteo.de>
2026-08-20 11:30:36 +0200

    docs(cpp01): edit inline comments in ex03

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 16:34:21 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/08/19 13:27:22 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/08/19 15:44:29 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 
 // _weapon starts at NULL
 HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
+
+// store the address of the referenced Weapon into the _weapon pointer
+// [Weapon &weapon] -> alias
+// [_weapon = &weapon] ->   takes its addres, assing it into the pointer
 void HumanB::setWeapon(Weapon &weapon) { _weapon = &weapon; }
 
-// must check _weapon before using it (it can be NULL)
-// otherwise dereferencing a NULL pointer is undefined behavior
+// check if _weapon is NULL, because dereferencing a NULL pointer is undefined
+// behavior
 void HumanB::attack() const {
     if (_weapon)
         std::cout << _name << " attacks with their " << _weapon->getType()
