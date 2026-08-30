1b86074763522e80883e77003df889196ebf8bca
Lucas Maia <lucasm@posteo.de>
2026-08-30 23:24:47 +0200

    docs(cpp02): edit inline comments

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,23 +6,21 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 09:42:48 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/08/23 22:48:02 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/08/30 23:24:29 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
-// Orthodox Canonical Form: default constructor, copy constructor\
-// copy assignment operator and destructor
-
+// Orthodox Canonical Form
 class Fixed {
 private:
     int _fixedPoint;                     // storing the raw fixed-point
     static const int fractionalBits = 8; // shared by all objects, never change
 
 public:
-    Fixed(void);                          // 1) constructor
+    Fixed(void);                          // 1) default constructor
     Fixed(const Fixed &other);            // 2) copy constructor
     Fixed &operator=(const Fixed &other); // 3) copy assignment operator
     ~Fixed();                             // 4) destructor
