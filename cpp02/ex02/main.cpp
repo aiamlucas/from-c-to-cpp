48f1cf2ace9af747d3efe9fe6d7c923aa0b437a5
Lucas Maia <lucasm@posteo.de>
2026-08-31 14:37:30 +0200

    docs(cpp02): edit inline comments

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 22:22:10 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/08/25 22:24:07 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/08/31 14:36:14 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 int main(void) {
     Fixed a;
     Fixed const b(Fixed(5.05f) * Fixed(2));
+    // 1) Fixed(5.05f)  — float constructor -> temp1 (raw = 1293)
+    // 2) Fixed(2)      — int constructor -> temp2   (raw = 512)
+    // 3) temp1 * temp2
+    // 4) Fixed const b(...) — copy constructor
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << a << std::endl;
