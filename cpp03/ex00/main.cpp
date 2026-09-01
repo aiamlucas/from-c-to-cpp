d6a71dab9003acb052612b374d70bbde5bbbdb35
Lucas Maia <lucasm@posteo.de>
2026-09-01 23:22:46 +0200

    feat(cpp03): update main.cpp

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,6 +6,16 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 16:30:03 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/09/01 16:30:04 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/09/01 19:21:32 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+int main(void) {
+    std::cout << "\n--- construction ---" << std::endl;
+    ClapTrap withoutName; // default constructor
+
+    return 0;
+}
