9f8c4474f73a70e1bac24acfe2d42fea837ab53f
Lucas Maia <lucasm@posteo.de>
2026-09-02 19:08:34 +0200

    feat(cpp03): update ex00

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 16:30:03 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/09/01 19:21:32 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/09/02 18:57:38 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,26 @@
 
 int main(void) {
     std::cout << "\n--- construction ---" << std::endl;
-    ClapTrap withoutName; // default constructor
+    ClapTrap noName;
+    ClapTrap insect("Mosquito");
+    ClapTrap copy(insect);
+    ClapTrap spider;
+    spider = insect;
 
+    std::cout << "\n--- normal attack - repair - damage ---" << std::endl;
+    insect.attack("Cat");
+    insect.beRepaired(3);
+    insect.takeDamage(2);
+
+    std::cout << "\n--- takeDamage underflow ---" << std::endl;
+    ClapTrap animal("Dog");
+    animal.takeDamage(9999); // hitpoints must clamp to 0
+    animal.attack("Human");  // should now do nothing.
+
+    std::cout << "\n--- energy drain guard ---" << std::endl;
+    ClapTrap drained("Drained");
+    for (int i = 0; i < 12; i++)
+        drained.attack("Target"); // only the first 10 should print
+    std::cout << "\n--- destruction ---" << std::endl;
     return 0;
 }
