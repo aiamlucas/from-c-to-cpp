928ee3c67502d1701668d08855ce9944e7830bfb
Lucas Maia <lucasm@posteo.de>
2026-09-13 21:32:25 +0200

    docs(cpp04): add inline comments ex04

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 18:07:11 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/09/09 11:34:26 by lbueno-m         ###   ########.fr       */
+/*   Updated: 2026/09/13 21:25:53 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,33 @@
 int main(void) {
 
     // subject example -> no leak on simple new/delete
+    // virtual destructor still works with Brain added
     {
         const Animal *j = new Dog();
         const Animal *i = new Cat();
 
         // deleting through Animal* (virtual destructor)
-        delete j; // should not create a leak
+        delete j; // ~Dog() -> delete it's brain -> ~Animal() no leak
         delete i;
     }
     // half Dog and half Cat
     std::cout << "\n--- array: Cat/Dog half half ---" << std::endl;
     Animal *animals[8];
     for (int i = 0; i < 8; i++) {
-        if (i < 4) // half of Dogs
-            animals[i] = new Dog();
-        else // half of Cats
+        if (i < 4)                  // half of Dogs
+            animals[i] = new Dog(); // each one allocate its own Brain
+        else                        // half of Cats
             animals[i] = new Cat();
     }
     for (int i = 0; i < 8; i++)
         delete animals[i]; // delete thorugh Animal* (virtual destructor)
-    // deep copy
+    // deep copy -> Brain must not be shared between the original and copy
     std::cout << "\n--- deep copy ---" << std::endl;
     {
         Cat juanCarlos;
         juanCarlos.getBrain()->setIdea(0, "I am hungry!");
         Cat cloneCat(juanCarlos); // copy constructor --> allocates a new Brain
+                                  // (ideas are copied)
         juanCarlos.getBrain()->setIdea(0, "I wanna sleep!");
         std::cout << "juanCarlos: " << juanCarlos.getBrain()->getIdea(0)
                   << std::endl; // "I wanna sleep!"
